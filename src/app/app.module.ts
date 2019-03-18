fd83f7b843bc15cb9592261fc20569eb98ae1b2f
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-18 17:03:48 +0000

    Refactored Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterModule } from './components/widgets/footer/footer.module';
+import { NavigationModule } from './components/widgets/navigation/navigation.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FooterModule, NavigationModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
