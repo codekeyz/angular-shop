559c0b73336def1eb3abbad47829b760b748ca55
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-21 12:06:13 +0000

    Fixed filter by range and sale

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService, Filter, SaleItem } from 'src/app/providers/data.service';
+import { Subject, Observable } from 'rxjs';
+import { map, switchMap, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  productsObservable: Observable<SaleItem[]>;
 
-  ngOnInit() {}
+  filter$ = new Subject<Filter>();
+  filter: Filter = {};
+
+  constructor(private dataSvc: DataService) {}
+
+  ngOnInit() {
+    this.productsObservable = this.filter$.pipe(
+      startWith(null),
+      map(res => {
+        this.filter = res || {};
+        return res;
+      }),
+      switchMap(res => {
+        return this.dataSvc.getObservaleProducts(res);
+      })
+    );
+  }
+
+  ontoggled(showOnlySale: boolean) {
+    this.filter.showOnlySale = showOnlySale;
+    this.filter$.next(this.filter);
+  }
+
+  rangeChanged(range: number) {
+    this.filter.highPrize = range;
+    this.filter$.next(this.filter);
+  }
 }
