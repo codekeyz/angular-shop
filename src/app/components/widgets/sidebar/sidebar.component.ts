596a455021a869343ca4a56aa0e46fb6c0ef77f9
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-21 12:05:45 +0000

    Refactored Sidebar COmponent

--- src/app/components/widgets/sidebar/sidebar.component.ts
+++ src/app/components/widgets/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,7 +12,13 @@ export class SidebarComponent implements OnInit {
 
   @Input() priceRange = 300;
 
+  @Output() rangeChange = new EventEmitter<any>();
+
   constructor() {}
 
   ngOnInit() {}
+
+  onchange($event) {
+    this.rangeChange.emit($event.srcElement.value);
+  }
 }
