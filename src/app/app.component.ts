225c1862937582e3b0b588c00f62b96650a0bf62
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-21 13:28:35 +0000

    show cart count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './providers/data.service';
+import { interval, of } from 'rxjs';
+import { startWith, switchMap, map } from 'rxjs/operators';
+import { SaleItem } from './providers/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-shoppity';
+export class AppComponent implements OnInit {
+  cartCount$;
+
+  constructor(private dataSvc: DataService) {}
+
+  ngOnInit() {
+    this.cartCount$ = interval(1000).pipe(
+      startWith(0),
+      switchMap(() => of(this.dataSvc.getCart())),
+      map(res => res.length)
+    );
+  }
 }
